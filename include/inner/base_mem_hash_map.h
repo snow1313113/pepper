225f0f00d2a878abeb54332b5dd6b26302046158
snow1313113 <59796717@qq.com>
2022-02-20 14:39:29 +0800

    add set refactor branch

---
+++ include/inner/base_mem_hash_map.h
@@ -0,0 +1,26 @@
+/*
+ * * file name: base_mem_hash_map.h
+ * * description:
+ * *     所有set 或者map类型的容器类的基础实现，用哈希桶实现，在少量数据的时候退化
+ * *     在数据量少的话退化成数组
+ * * author: snow
+ * * create time:2022  2 20
+ * */
+
+#ifndef _BASE_MEM_HASH_MAP_H_
+#define _BASE_MEM_HASH_MAP_H_
+
+#include <iterator>
+#include <utility>
+#include "../utils/traits_utils.h"
+#include "head.h"
+
+namespace pepper
+{
+namespace inner
+{
+    // todo
+}
+}  // namespace pepper
+
+#endif
