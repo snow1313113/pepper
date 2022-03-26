f28e933d05c92c5db2ec4b1cd3f66284b6fc184b
snow1313113 <59796717@qq.com>
2022-03-26 16:38:31 +0800

    use LRUPolicy

--- include/mem_lru_set.h
+++ include/mem_lru_set.h
@@ -8,14 +8,16 @@
 #ifndef _MEM_LRU_SET_H_
 #define _MEM_LRU_SET_H_
 
-#include "inner/base_mem_lru_map.h"
+//#include "inner/base_mem_lru_map.h"
 #include "inner/base_specialization.h"
-#include "inner/policy.h"
+//#include "inner/policy.h"
+#include "inner/lru_policy.h"
+#include "inner/new_base_lru_map.h"
 
 namespace pepper
 {
-template <typename T, size_t MAX_SIZE, typename POLICY = BasePolicy<T>>
-using MemLRUSet = inner::BaseMemLRUMap<T, void, MAX_SIZE, POLICY>;
+template <typename T, size_t MAX_SIZE>
+using MemLRUSet = exp::BaseMemLRUMap<inner::LRUPolicy<T, void, MAX_SIZE>>;
 
 }  // namespace pepper
 
