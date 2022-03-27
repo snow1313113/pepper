842a7b23bcea01d601bc14a48d34b89470caece0
snow1313113 <59796717@qq.com>
2022-03-27 11:11:07 +0800

    change test file from .h to .cpp

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
-#include "fixed_mem_pool_test.h"
-#include "hash_mem_pool_test.h"
-#include "mem_lru_map_test.h"
-#include "mem_lru_set_test.h"
-#include "mem_map_test.h"
-#include "mem_set_test.h"
-#include "ring_buffer_test.h"
+#include "gtest/gtest.h"
 
 int main(int argc, char* argv[])
 {
